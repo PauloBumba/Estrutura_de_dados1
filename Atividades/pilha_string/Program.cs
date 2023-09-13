4e7ff512b327606aefecc1802ce69d2e6562db8b
Paulo418566 <paulomvbumba@gmail.com>
2023-09-13 13:09:53 -0300

    Pilha String

---
+++ Atividades/pilha_string/Program.cs
@@ -0,0 +1,10 @@
+﻿using StackClass;
+
+Stack pilha = new Stack(); 
+
+pilha.Push("Paulo");
+pilha.Push("Mário");
+pilha.Push("Valente");
+pilha.Push("Bumba");
+
+pilha.PrintStack();
---
+++ Atividades/pilha_string/pilha.cs
@@ -0,0 +1,69 @@
+using System;
+
+namespace StackClass
+{
+    public class Stack
+    {
+        static readonly int MAX = 1000;
+        int top = -1;
+        string[] stack = new string[MAX]; 
+
+        public bool IsEmpty()
+        {
+            return (top < 0);            
+        }
+
+        public bool Push(string data)
+        {
+            if(top >= MAX)
+            {
+                Console.WriteLine("Stack Overflow");
+                return false;
+            }
+            top += 1;
+            stack[top] = data;
+            return true;
+        }
+
+        public string Pop() 
+        {
+            if(top < 0)
+            {
+                Console.WriteLine("Stack Underflow");
+                return null;
+            }
+
+            string valor = stack[top];            
+            top--;
+
+            return valor;
+        }
+
+        public void Peek()
+        {
+            if(top < 0)
+            {
+                Console.WriteLine("Stack Underflow");
+                return;
+            }
+
+            Console.WriteLine($"O topo da pilha é: {stack[top]}" );
+        }
+
+        public void PrintStack()
+        {
+            if(top < 0)
+            {
+                Console.WriteLine("Stack Underflow");
+                return;
+            }
+
+            Console.WriteLine("Itens da pilha:");
+            for(int i = top; i >= 0; i-- )
+            {
+                string texto = $"Stack[{ stack[i] }]";
+                Console.WriteLine(texto);
+            }
+        }
+    }
+}
