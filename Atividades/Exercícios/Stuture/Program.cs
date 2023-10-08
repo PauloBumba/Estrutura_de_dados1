97549adc1a5d8590d7b9329562ca472d5bfc7f31
Paulo418566 <paulomvbumba@gmail.com>
2023-10-08 19:46:56 -0300

    paulo

---
+++ Atividades/Exercícios/Stuture/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+
+// Classe que representa um departamento.
+class Department
+{
+    public string Name { get; set; }
+    public List<Department> SubDepartments { get; set; }
+
+    public Department(string name)
+    {
+        Name = name;
+        SubDepartments = new List<Department>();
+    }
+
+    public void AddSubDepartment(Department subDepartment)
+    {
+        SubDepartments.Add(subDepartment);
+    }
+
+    public void Display(int level = 0)
+    {
+        string indent = new string(' ', level * 4);
+        Console.WriteLine(indent + Name);
+        foreach (var subDepartment in SubDepartments)
+        {
+            subDepartment.Display(level + 1);
+        }
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        // Crie os departamentos
+        Department ceo = new Department("CEO");
+        Department cto = new Department("CTO");
+        Department hr = new Department("Recursos Humanos");
+        Department engineering = new Department("Engenharia");
+        Department marketing = new Department("Marketing");
+
+        // Construa a hierarquia
+        ceo.AddSubDepartment(cto);
+        ceo.AddSubDepartment(hr);
+        cto.AddSubDepartment(engineering);
+        cto.AddSubDepartment(marketing);
+
+        // Exiba o organograma
+        ceo.Display();
+    }
+}
